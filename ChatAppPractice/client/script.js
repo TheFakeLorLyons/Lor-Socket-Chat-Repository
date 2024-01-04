85d450eb846036380263e114d0426a3891edb3f2
TheFakeLorLyons <lorelailyons@protonmail.com>
2024-01-04 01:08:18 -0500

    First module completed

--- ChatAppPractice/client/script.js
+++ ChatAppPractice/client/script.js
@@ -5,7 +5,14 @@ const messageInput = document.getElementById("message-input")
 const roomInput = document.getElementById("room-input")
 const form = document.getElementById("form")
 
-const socket = io("http://localhost:3000")
+const socket = io('http://localhost:3000')
+const userSocket = io("http://localhost:3000/user", { auth: {token: "Test"} //if we dont check the auth or they dont pass then the below code will run
+})
+
+//userSocket.on('connect_error", error => {
+    //displayMessage(error)                                 //For instructional use
+//})
+
 socket.on("connect", () => {
     displayMessage(`You connected with id: ${socket.id}`)   //Message displayed to CLIENT (defined by us below)
     socket.emit('custom-event', 10, 'Hi', {a: 'a'})         //sends an event out to the SERVER
@@ -40,3 +47,16 @@ function displayMessage(message) {
     div.textContent = message
     document.getElementById("message-container").append(div)
 }
+
+
+let count = 0
+setInterval(() => {
+    socket.volatile.emit("ping", ++count) //okay so volatile will make it so no messages are passed if there is a disconnection with the server.
+}, 1000)                                    //if removed, the code wil send all the messages that were sent in the interim all at once upon reconnection.
+
+document.addEventListener("keydown", e => {
+    if (e.target.matches("input")) return
+
+    if (e.key === "c") socket.connect()
+    if (e.key === "d") socket.disconnect()
+})
--- ChatAppPractice/server/server.js
+++ ChatAppPractice/server/server.js
@@ -1,8 +1,30 @@
+const { instrument } = require("@socket.io/admin-ui")
+
 const io = require("socket.io")(3000, {
-    cors: {
-        origin: ["http://localhost:8080"],
-    },
+    cors: { 
+        allowEIO3:true,
+        origin: ["http://localhost:8080", "https://admin.socket.io"],
+        credentials:true
+    }
+});
+
+const userIo = io.of('/user')
+userIo.on("connection", socket => {
+    console.log("connected to user namespace with username " + socket.username)
 })
+
+userIo.use((socket, next) => {
+    if(socket.handshake.auth.token) {
+        socket.username = getUsernameFromToken(socket.handshake.auth.token)
+        next()
+    } else {
+        next(new Error('Please send token'))
+    }
+})//this and userIo above can be used to simply authenticate user
+
+function getUsernameFromToken(token) {
+    return token //this is where we could access information from the DB
+}
 
 /*io.on("connection", socket => {
     console.log(socket.id)
@@ -15,15 +37,18 @@ io.on("connection", socket => {
     console.log(socket.id)                  //every user in socket is in a room of their own that is by default their own userid
     socket.on('send-message', (message, room) => {
         if(room === '') {
-            socket.broadcast.emit('receive-message', message) //io.emit sends to all users, io.broadcast.emit sends to all users except the sender.
+            socket.broadcast.emit("receive-message", message) //io.emit sends to all users, io.broadcast.emit sends to all users except the sender.
             console.log(message)
         } else {
             //socket.to(room).emit("receive-message", socket.id)
             socket.to(room).emit("receive-message", message)//the 'socket.to' implicitly assumes '.broadcast.to' and does not send to sender.
         }
     })
-    socket.on('join-room', (room, cb) => { //must always end a callback function with a cb if applicable
+    socket.on("join-room", (room, cb) => { //must always end a callback function with a cb if applicable
         socket.join(room)
         cb(`Joined ${room}`)
     })
+    socket.on("ping", n => console.log(n))
 })
+
+instrument(io, {withCredentials: true,auth: false})
