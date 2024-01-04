4bb3ea656cb40b34eb649b7ecc35223197d41490
TheFakeLorLyons <lorelailyons@protonmail.com>
2024-01-03 22:31:41 -0500

    Can join rooms now.

--- ChatAppPractice/server/server.js
+++ ChatAppPractice/server/server.js
@@ -4,17 +4,26 @@ const io = require("socket.io")(3000, {
     },
 })
 
-io.on("connection", socket => {
+/*io.on("connection", socket => {
     console.log(socket.id)
     socket.on('custom-event', (number, string, object,) => {
         console.log(number, string, object)
     })
-})
+})*/
 
 io.on("connection", socket => {
-    console.log(socket.id)
-    socket.on('send-message', message => {
-        io.emit('receive-message', message)
-        console.log(message)
+    console.log(socket.id)                  //every user in socket is in a room of their own that is by default their own userid
+    socket.on('send-message', (message, room) => {
+        if(room === '') {
+            socket.broadcast.emit('receive-message', message) //io.emit sends to all users, io.broadcast.emit sends to all users except the sender.
+            console.log(message)
+        } else {
+            //socket.to(room).emit("receive-message", socket.id)
+            socket.to(room).emit("receive-message", message)//the 'socket.to' implicitly assumes '.broadcast.to' and does not send to sender.
+        }
+    })
+    socket.on('join-room', (room, cb) => { //must always end a callback function with a cb if applicable
+        socket.join(room)
+        cb(`Joined ${room}`)
     })
 })
